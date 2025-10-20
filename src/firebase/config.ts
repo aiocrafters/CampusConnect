c9b7693240d35190cf04e64e2f02f8dcef8fdacf
OSM <contactjkosm@gmail.com>
2025-10-20 13:54:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-351748969-9aeec",
+  "appId": "1:752051550171:web:0f7d8b96b73b3c443259fe",
+  "apiKey": "AIzaSyDje2yHGyREjHwmemdw-rB_opjcQ2yG-mk",
+  "authDomain": "studio-351748969-9aeec.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "752051550171"
+};
